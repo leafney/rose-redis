87cf01ec64e57e68a0d2ba51084b3c3d4fdef7cc
leafney <babycoolzx@126.com>
2023-03-27 01:17:07 +0800

    feat: hash methods

--- redis.go
+++ redis.go
@@ -103,7 +103,7 @@ func NewRedis(addr string, opts ...Option) (*Redis, error) {
 }
 
 func newRedis(addr string, opts ...Option) *Redis {
-	// 初始化默认配置
+	// Initial configuration
 	r := &Redis{
 		Addr: addr,
 		Type: NodeType,
@@ -111,12 +111,12 @@ func newRedis(addr string, opts ...Option) *Redis {
 		Pass: "",
 	}
 
-	// 载入用户配置
+	// load user configuration
 	for _, opt := range opts {
 		opt(r)
 	}
 
-	// 是否集群判断
+	// is cluster mode
 	r.isCluster = r.Type == ClusterType
 
 	return r
@@ -282,13 +282,13 @@ func (s *Redis) DecrCtx(ctx context.Context, key string) (val int64, err error) 
 	return s.client.Decr(ctx, key).Result()
 }
 
-// Decrby is the implementation of redis decrby command.
-func (s *Redis) Decrby(key string, decrement int64) (int64, error) {
-	return s.DecrbyCtx(context.Background(), key, decrement)
+// DecrBy is the implementation of redis decrby command.
+func (s *Redis) DecrBy(key string, decrement int64) (int64, error) {
+	return s.DecrByCtx(context.Background(), key, decrement)
 }
 
-// DecrbyCtx is the implementation of redis decrby command.
-func (s *Redis) DecrbyCtx(ctx context.Context, key string, decrement int64) (val int64, err error) {
+// DecrByCtx is the implementation of redis decrby command.
+func (s *Redis) DecrByCtx(ctx context.Context, key string, decrement int64) (val int64, err error) {
 	return s.client.DecrBy(ctx, key, decrement).Result()
 }
 
@@ -350,13 +350,13 @@ func (s *Redis) ExpireCtx(ctx context.Context, key string, seconds int64) error 
 	return s.client.Expire(ctx, key, time.Duration(seconds)*time.Second).Err()
 }
 
-// Expireat is the implementation of redis expireat command.
-func (s *Redis) Expireat(key string, expireTime int64) error {
-	return s.ExpireatCtx(context.Background(), key, expireTime)
+// ExpireAt is the implementation of redis expireat command.
+func (s *Redis) ExpireAt(key string, expireTime int64) error {
+	return s.ExpireAtCtx(context.Background(), key, expireTime)
 }
 
-// ExpireatCtx is the implementation of redis expireat command.
-func (s *Redis) ExpireatCtx(ctx context.Context, key string, expireTime int64) error {
+// ExpireAtCtx is the implementation of redis expireat command.
+func (s *Redis) ExpireAtCtx(ctx context.Context, key string, expireTime int64) error {
 	return s.client.ExpireAt(ctx, key, time.Unix(expireTime, 0)).Err()
 }
 
@@ -479,5 +479,165 @@ func (s *Redis) GetSetCtx(ctx context.Context, key, value string) (val string, e
 	}
 	return "", err
 }
+
+// ------------------------
+
+// HDel is the implementation of redis hdel command.
+func (s *Redis) HDel(key string, fields ...string) (bool, error) {
+	return s.HDelCtx(context.Background(), key, fields...)
+}
+
+// HDelCtx is the implementation of redis hdel command.
+func (s *Redis) HDelCtx(ctx context.Context, key string, fields ...string) (val bool, err error) {
+	v, err := s.client.HDel(ctx, key, fields...).Result()
+	return v >= 1, err
+}
+
+// HExists is the implementation of redis hexists command.
+func (s *Redis) HExists(key, field string) (bool, error) {
+	return s.HExistsCtx(context.Background(), key, field)
+}
+
+// HExistsCtx is the implementation of redis hexists command.
+func (s *Redis) HExistsCtx(ctx context.Context, key, field string) (val bool, err error) {
+	return s.client.HExists(ctx, key, field).Result()
+}
+
+// HGet is the implementation of redis hget command.
+func (s *Redis) HGet(key, field string) (string, error) {
+	return s.HGetCtx(context.Background(), key, field)
+}
+
+// HGetCtx is the implementation of redis hget command.
+func (s *Redis) HGetCtx(ctx context.Context, key, field string) (val string, err error) {
+	return s.client.HGet(ctx, key, field).Result()
+}
+
+// HGetAll is the implementation of redis hgetall command.
+func (s *Redis) HGetAll(key string) (map[string]string, error) {
+	return s.HGetAllCtx(context.Background(), key)
+}
+
+// HGetAllCtx is the implementation of redis hgetall command.
+func (s *Redis) HGetAllCtx(ctx context.Context, key string) (val map[string]string, err error) {
+	return s.client.HGetAll(ctx, key).Result()
+}
+
+// HIncrBy is the implementation of redis hincrby command.
+func (s *Redis) HIncrBy(key, field string, increment int64) (int64, error) {
+	return s.HIncrByCtx(context.Background(), key, field, increment)
+}
+
+// HIncrByCtx is the implementation of redis hincrby command.
+func (s *Redis) HIncrByCtx(ctx context.Context, key, field string, increment int64) (val int64, err error) {
+	return s.client.HIncrBy(ctx, key, field, increment).Result()
+}
+
+// HIncrByFloat is the implementation of redis hincrbyfloat command.
+func (s *Redis) HIncrByFloat(key, field string, increment float64) (float64, error) {
+	return s.HIncrByFloatCtx(context.Background(), key, field, increment)
+}
+
+// HIncrByFloatCtx is the implementation of redis hincrbyfloat command.
+func (s *Redis) HIncrByFloatCtx(ctx context.Context, key, field string, increment float64) (val float64, err error) {
+	return s.client.HIncrByFloat(ctx, key, field, increment).Result()
+}
+
+// HKeys is the implementation of redis hkeys command.
+func (s *Redis) HKeys(key string) ([]string, error) {
+	return s.HKeysCtx(context.Background(), key)
+}
+
+// HKeysCtx is the implementation of redis hkeys command.
+func (s *Redis) HKeysCtx(ctx context.Context, key string) (val []string, err error) {
+	return s.client.HKeys(ctx, key).Result()
+}
+
+// HLen is the implementation of redis hlen command.
+func (s *Redis) HLen(key string) (int64, error) {
+	return s.HLenCtx(context.Background(), key)
+}
+
+// HLenCtx is the implementation of redis hlen command.
+func (s *Redis) HLenCtx(ctx context.Context, key string) (val int64, err error) {
+	return s.client.HLen(ctx, key).Result()
+}
+
+// HMGet is the implementation of redis hmget command.
+func (s *Redis) HMGet(key string, fields ...string) ([]string, error) {
+	return s.HMGetCtx(context.Background(), key, fields...)
+}
+
+// HMGetCtx is the implementation of redis hmget command.
+func (s *Redis) HMGetCtx(ctx context.Context, key string, fields ...string) (val []string, err error) {
+	v, err := s.client.HMGet(ctx, key, fields...).Result()
+	val = toStrings(v)
+	return
+}
+
+// HSet is the implementation of redis hset command.
+func (s *Redis) HSet(key, field, value string) error {
+	return s.HSetCtx(context.Background(), key, field, value)
+}
+
+// HSetCtx is the implementation of redis hset command.
+func (s *Redis) HSetCtx(ctx context.Context, key, field, value string) error {
+	return s.client.HSet(ctx, key, field, value).Err()
+}
+
+// HSetNX is the implementation of redis hsetnx command.
+func (s *Redis) HSetNX(key, field, value string) (bool, error) {
+	return s.HSetNXCtx(context.Background(), key, field, value)
+}
+
+// HSetNXCtx is the implementation of redis hsetnx command.
+func (s *Redis) HSetNXCtx(ctx context.Context, key, field, value string) (val bool, err error) {
+	return s.client.HSetNX(ctx, key, field, value).Result()
+}
+
+// HMSet is the implementation of redis hmset command.
+func (s *Redis) HMSet(key string, fieldsAndValues map[string]string) error {
+	return s.HMSetCtx(context.Background(), key, fieldsAndValues)
+}
+
+// HMSetCtx is the implementation of redis hmset command.
+func (s *Redis) HMSetCtx(ctx context.Context, key string, fieldsAndValues map[string]string) error {
+	vals := make(map[string]interface{}, len(fieldsAndValues))
+	for k, v := range fieldsAndValues {
+		vals[k] = v
+	}
+
+	return s.client.HMSet(ctx, key, vals).Err()
+}
+
+// HScan is the implementation of redis hscan command.
+func (s *Redis) HScan(key string, cursor uint64, match string, count int64) (
+	keys []string, cur uint64, err error) {
+	return s.HScanCtx(context.Background(), key, cursor, match, count)
+}
+
+// HScanCtx is the implementation of redis hscan command.
+func (s *Redis) HScanCtx(ctx context.Context, key string, cursor uint64, match string, count int64) (
+	keys []string, cur uint64, err error) {
+	keys, cur, err = s.client.HScan(ctx, key, cursor, match, count).Result()
+	return
+}
+
+// HVals is the implementation of redis hvals command.
+func (s *Redis) HVals(key string) ([]string, error) {
+	return s.HValsCtx(context.Background(), key)
+}
+
+// HValsCtx is the implementation of redis hvals command.
+func (s *Redis) HValsCtx(ctx context.Context, key string) (val []string, err error) {
+	val, err = s.client.HVals(ctx, key).Result()
+	return
+}
+
+// ------------------------
+
+// ------------------------
+
+// ------------------------
 
 // ------------------------
